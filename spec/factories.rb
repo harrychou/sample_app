30af921e73c11def48e85f27c134fedc2439c4ee
harrychou <harrychou@gmail.com>
2011-05-24 21:03:57 -0400

    Done with user edit/update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,11 @@
 Factory.define :user do |user|
-  user.name "Harry Chou"
-  user.email "harrychou@gmail.com"
-  user.password "foobar"
+  user.name                  "Michael Hartl"
+  user.email                 "mhartl@example.com"
+  user.password              "foobar"
   user.password_confirmation "foobar"
 end
+
+Factory.sequence :email do |n|
+  "person-#{n}@example.com"
+end
+
